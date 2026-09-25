621a124ef2041ace30d10b519d471ac9b09461be
Pratyush Kumar Mishra <neel@Pratyushs-MacBook-Air.local>
2026-09-25 10:05:59 +0530

    Read bug updated

---
+++ app/terms/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Contact from '@/components/Contact'
+function page() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default page 
